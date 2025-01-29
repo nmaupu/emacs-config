68144d08960626207dd2ea6e81431bbf6857e5c4
Nicolas Maupu <nicolas.maupu.ext@ovhcloud.com>
2025-01-29 17:36:11 +0100

    Generated files

--- custom.el
+++ custom.el
@@ -3,10 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" default))
+ '(inhibit-startup-screen t)
+ '(lsp-ui-imenu-enable t)
+ '(package-selected-packages
+   '(treemacs dap-mode flycheck-golangci-lint projectile flx-ido yasnippet use-package lsp-ui go-mode flycheck company-lsp))
  '(safe-local-variable-values
    '((vc-prepare-patches-separately)
      (diff-add-log-use-relative-names . t)
-     (vc-git-annotate-switches . "-w"))))
+     (vc-git-annotate-switches . "-w")))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
